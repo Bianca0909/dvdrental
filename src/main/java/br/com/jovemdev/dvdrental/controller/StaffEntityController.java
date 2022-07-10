c5dbafa5b5244041957d7b41b1e49cf4d2ee52f5
Bianca0909 <biancamedeiros0909@gmail.com>
2022-07-09 21:50:17 -0300

    new commit

---
+++ src/main/java/br/com/jovemdev/dvdrental/controller/StaffEntityController.java
@@ -0,0 +1,33 @@
+package br.com.jovemdev.dvdrental.controller;
+
+import br.com.jovemdev.dvdrental.entity.StaffEntity;
+import br.com.jovemdev.dvdrental.service.StaffEntityService;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/staff")
+public class StaffEntityController {
+    @Autowired
+    private StaffEntityService service;
+
+    @GetMapping("/listar")
+    public List<StaffEntity> lista() {
+        return service.findAll();
+    }
+    @GetMapping("/{id}")
+    public StaffEntity buscarporId(@PathVariable Long id) {
+        return service.findById(id);
+    }
+    @PostMapping()
+    public StaffEntity salvar(@RequestBody StaffEntity staffEntity) {
+        return service.save(staffEntity);
+    }
+    @GetMapping("/name/{name}")
+    public List<StaffEntity> findByFirstNameContains(@PathVariable String name) {
+        return service.findByFirstNameContains(name);
+    }
+}
